c50789a293deb9b8ff573b216062d3ddab2830af
5f1839842f2fb <57242175+5f1839842f2fb@users.noreply.github.com>
2020-02-28 00:17:21 -0500

    Fixed some issues page styling

--- app/src/dummydata.js
+++ app/src/dummydata.js
@@ -9,35 +9,35 @@ export default [
   {
     title: "Potholes",
     description: "Fix the potholes please",
-    img: "https://media1.fdncms.com/chicago/imager/u/blog/71086724/potholes010.jpg?cb=1560878940",
+    img: "https://cdn.vox-cdn.com/thumbor/SyY1cHnoIgaGMO7F0Wfb6n4_LMg=/532x532:3436x2372/920x613/filters:focal(1479x1201:2119x1841):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/55271881/Newport_Whitepit_Lane_pot_holes_2.0.jpeg",
     upvotes: Math.round(Math.random()*100),
     zipcode: Math.round(Math.random()*100000).toString()
   },
   {
     title: "Potholes",
     description: "Fix the potholes please",
-    img: "https://media1.fdncms.com/chicago/imager/u/blog/71086724/potholes010.jpg?cb=1560878940",
+    img: "https://www.allstate.com/blog/wp-content/uploads/2019/02/pothole-in-street-gettyimages-1200x675-780x387.jpg",
     upvotes: Math.round(Math.random()*100),
     zipcode: Math.round(Math.random()*100000).toString()
   },
   {
     title: "Potholes",
     description: "Fix the potholes please",
-    img: "https://media1.fdncms.com/chicago/imager/u/blog/71086724/potholes010.jpg?cb=1560878940",
+    img: "https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/f_auto,q_auto,w_1100/v1555441386/shape/mentalfloss/istock_000000234266_small.jpg",
     upvotes: Math.round(Math.random()*100),
     zipcode: Math.round(Math.random()*100000).toString()
   },
   {
     title: "Potholes",
     description: "Fix the potholes please",
-    img: "https://media1.fdncms.com/chicago/imager/u/blog/71086724/potholes010.jpg?cb=1560878940",
+    img: "https://townsquare.media/site/696/files/2019/02/Potholes.jpg?w=980&q=75",
     upvotes: Math.round(Math.random()*100),
     zipcode: Math.round(Math.random()*100000).toString()
   },
   {
     title: "Potholes",
     description: "Fix the potholes please",
-    img: "https://media1.fdncms.com/chicago/imager/u/blog/71086724/potholes010.jpg?cb=1560878940",
+    img: "https://my1053wjlt.com/files/2017/01/pothole-Maudib.jpg?w=980&q=75",
     upvotes: Math.round(Math.random()*100),
     zipcode: Math.round(Math.random()*100000).toString()
   },
